e54c3c327aa230418225ada127ac625fd4f7cdf5
BiliBiliBaiye_CN <89338381+BiliBiliBaiyeCN@users.noreply.github.com>
2022-12-30 10:57:48 +0800

    Sync Update [Update_KBm_p_t_g_1]
    Sync the java class to EP.1 on YouTube

---
+++ src/me/tutorial/tutorial/Main.java
@@ -0,0 +1,18 @@
+package me.tutorial.tutorial;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&a[Tutorial] Plugin loaded."));
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&a[Tutorial] Plugin loaded."));
+    }
+}
